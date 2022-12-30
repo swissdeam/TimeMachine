61ca20c4da74f584e54c141ac7e3bb36f5f78cd3
Pavel Mineev <swissdeam@gmail.com>
2022-12-30 05:32:59 +0400

    transfer index to root

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ module.exports = {
     entry: './src/app.js',
     output: {
         filename: 'bundle.[chunk-hash].js',
-        path: path.resolve(__dirname, 'public')
     },
     devServer: {
         port: 3000
